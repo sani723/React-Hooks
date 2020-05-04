8bbb8e7c3f3871e2e0194615c88dd47182138a69
Sajjad Haider <sajjad.haider@accenture.com>
2020-05-04 23:44:55 +0400

    udpated Modal logic

--- src/Modal.js
+++ src/Modal.js
@@ -1,2 +1,22 @@
-import React, { useEffect, useRef } from 'react';
-import { createPortal } from 'react-dom';
+import React, { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Modal = ({ children }) => {
+  const elRef = useRef(null);
+
+  if (!elRef.current) {
+    const modalWrapper = document.createElement("div");
+    elRef.current = modalWrapper;
+  }
+
+  useEffect(() => {
+    const modalRoot = document.getElementById("modal");
+    modalRoot.appendChild(elRef.current);
+
+    return () => modalRoot.removeChild(elRef.current);
+  }, []);
+
+  return createPortal(<div>{children}</div>, elRef.current);
+};
+
+export default Modal;
