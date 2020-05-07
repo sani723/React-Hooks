b489c99aacefe683bb1ec8a830fb7eb59b271eff
Sajjad Haider <sajjad.haider@accenture.com>
2020-05-07 19:28:10 +0400

    Add NavBar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import Posts from "./Posts";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 import ThemeContext from "./ThemeContext";
 import Hooks from "./Hooks";
+import NavBar from './NavBar';
 
 const App = () => {
   const themeHook = useState("orange");
@@ -13,9 +14,7 @@ const App = () => {
     <React.StrictMode>
       <ThemeContext.Provider value={themeHook}>
         <div>
-          <header>
-            <Link to="/">Adopt ME</Link>
-          </header>
+          <NavBar />
           <Router>
             <SearchParams path="/" />
             <Details path="/details/:id" />
